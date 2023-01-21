887257b8459b7b878a20e295682f37def5563333
tombs4 <thomas.s.r.bolton@gmail.com>
2023-01-21 15:48:54 -0500

    updates to use different api

---
+++ kayak/config.py
@@ -0,0 +1,6 @@
+class config:
+    def __init__(self) -> None:
+        # latitude for Frederick MD
+        self.lat = "39.4143"
+        # longitude for Frederick MD
+        self.long = "-77.4105"
--- kayak/kayak.py
+++ kayak/kayak.py
@@ -1,22 +1,46 @@
 import requests
-# gets the API key so that it's not hard-coded into github
-with open('c:\\users\\thoma\\kayak_api_key.txt', 'r') as f:
-    key = f.read()
+import json
+import config
+
 # latitude for Frederick MD
-lat = "39.4143"
+fred = config.config() 
+print(fred.lat)
 # longitude for Frederick MD
-lon = "77.4105"
-# the URL that will be used to get the data. 2.5 spcifies less data will be retrieved
-# this is in keeping with the free version that is being used
-url = "https://api.openweathermap.org/data/2.5/onecall?lat={}&lon={}&appid={}".format(lat,lon,key)
+print(fred.long)
+
+# using weather.gov API because it's free.
+url = f"https://api.weather.gov/points/{fred.lat},{fred.long}"
 # prints the filled in URL for troubleshooting
 print(url)
 # get the response from the API
 response = requests.get(url)
 # show the response from the API
-print(response.content)
+# print(response.json())
 
-# at time of writing this was throwing a 401 response
-# b'{"cod":401, "message": "Invalid API key. Please see http://openweathermap.org/faq#error401 for more info."}'
-# there's an error that indicates that it will take several hours for the API key to activate
-# pushing this commit and going to bed
+# there should probably be a test for the below response 
+# to verify that it's returning a real result
+jsonresponse = response.json()
+json_pretty = json.dumps(jsonresponse, indent=4)
+# print(json_pretty)
+# print(jsonresponse)
+
+# used to get the properties in an easy-to-read format
+# json_output = open("c:\\users\\thoma\\Git\\kayak\\json_response.json", "w")
+# json_output.write(json_pretty)
+
+# response = json.loads(jsonresponse)
+# print(jsonresponse.items())
+forecast_api_url = jsonresponse["properties"]["forecastHourly"]
+print(forecast_api_url)
+
+forecast_response = requests.get(forecast_api_url)
+print(forecast_response.status_code)
+if forecast_response.status_code == 200:
+    json_output = open("c:\\users\\thoma\\Git\\kayak\\forecast_json.json", "w")
+    json_output.write(json.dumps(forecast_response.json(), indent=4))
+else:
+    print("\n\nthat didn't work")
+
+# well, the good news is that I found the data that I need in the 
+# forecast_json.json file. Now i just need to figure out the appropriate algorithm
+# for determining when it will be good to go kayaking in the next few days
